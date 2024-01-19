57b95d6d0540b4e7a56db1c417102ee3324f5fd5
xvacorx <theproofthevictors@gmail.com>
2024-01-18 21:29:11 -0300

    2commit

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] enemyPrefab;
+    int minEnemigos = 4;
+    int maxEnemigos = 7;
+
+    public float radioMinimo = 10f;
+    public float RadioMaximo = 25f;
+
+    public void Spawn ()
     {
-        
+        int numEnemigos = Random.Range(minEnemigos, maxEnemigos + 1);
+
+        for (int i = 0; i < numEnemigos; i++)
+        {
+            generarEnemigos();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void generarEnemigos()
     {
-        
+        GameObject enemigoPrefab = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
+
+        float angulo = Random.Range(0f, 2f * Mathf.PI);
+        float radio = Random.Range(radioMinimo, RadioMaximo);
+        float x = transform.position.x + Mathf.Cos(angulo) * radio;
+        float z = transform.position.z + Mathf.Sin(angulo) * radio;
+        float y = transform.position.z + Random.Range(1f, 10f);
+
+        Vector3 posicionEnemigo = new Vector3 (x, y, z);
+        Instantiate(enemigoPrefab, posicionEnemigo, Quaternion.identity);
+
     }
+
+
 }
