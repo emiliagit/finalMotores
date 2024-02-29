151a400767981c850b8c735ed8c258231f7b5cdd
xvacorx <theproofthevictors@gmail.com>
2024-02-29 16:16:29 -0300

    compass

---
+++ Assets/compass.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class compass : MonoBehaviour
+{
+
+    float distanceFromPlayer = 4f;
+
+    Transform Player;
+    Transform targetMision;
+    float heightAboveGround = 1f;
+
+    public bool compassActive = true;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        FindClosestMision();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+           
+            compassActive = true;
+        }
+
+        if (compassActive)
+        {
+            FindClosestMision();
+
+            float distanceMision = Vector3.Distance(Player.position, targetMision.position);
+
+
+            Vector3 targetPosition = new Vector3(targetMision.position.x, Player.position.y + heightAboveGround, targetMision.position.z);
+            Vector3 directionMision = targetPosition - Player.position;
+            Vector3 desiredPosition = Player.position + directionMision.normalized * distanceFromPlayer;
+
+
+            transform.LookAt(targetPosition);
+            transform.position = desiredPosition;
+        }
+
+        
+    }
+
+    void FindClosestMision()
+    {
+        GameObject[] misiones = GameObject.FindGameObjectsWithTag("Misiones");
+        float closestDistance = Mathf.Infinity;
+
+        if (misiones.Length > 0)
+        {
+            foreach (GameObject mision in misiones)
+            {
+                float distance = Vector3.Distance(Player.position, mision.transform.position);
+
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    targetMision = mision.transform;
+                }
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
