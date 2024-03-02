d0e8fdf313c50acca4a465d687ed83b2ddb4b881
xvacorx <theproofthevictors@gmail.com>
2024-03-02 17:40:12 -0300

    victory

---
+++ Assets/Game/scripts/VictoriaDeJuego.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoriaDeJuego : MonoBehaviour
+{
+
+    public KeyCode targetKey = KeyCode.E;
+
+    public float requiredHoldTime = 3.0f;
+
+    private float currentHoldTime = 0.0f;
+    private bool victoryAchieved = false;
+
+    public TextMeshProUGUI countdownText;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(targetKey))
+        {
+            currentHoldTime += Time.deltaTime;
+
+            // Si se mantiene presionada durante el tiempo requerido, activa la victoria
+            if (currentHoldTime >= requiredHoldTime && !victoryAchieved)
+            {
+                victoryAchieved = true;
+                gameManager.Instance.ShowWinScreen();
+            }
+        }
+        else
+        {
+            // Reinicia el contador si la tecla se suelta antes de tiempo
+            currentHoldTime = 0.0f;
+        }
+
+        UpdateCountdownText();
+    }
+
+    void UpdateCountdownText()
+    {
+        float remainingTime = Mathf.Max(0f, requiredHoldTime - currentHoldTime);
+        countdownText.text = $"Tiempo restante: {remainingTime:F1} s";
+    }
+
+
+}
+
